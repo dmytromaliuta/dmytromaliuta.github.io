16c4e9db96df8e8b33aaaea8805fef03ff8e99ba
Dmytro <dmytromaliuta@gmail.com>
2019-04-08 16:11:31 +0300

    s

--- mail/uniMail-master/script.js
+++ mail/uniMail-master/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
 	//E-mail Ajax Send
-	$("form").submit(function() { //Change
+	$("modal__form").submit(function() { //Change
 		var th = $(this);
 		$.ajax({
 			type: "POST",
